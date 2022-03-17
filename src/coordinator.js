fa942bcce72ece8445487fa3fa0612173606b7c3
Andrew <andrew.montenigro@gmail.com>
2022-03-16 19:10:07 -0500

    feat: implement friend list fetch/search

--- src/coordinator.js
+++ src/coordinator.js
@@ -3,8 +3,37 @@ require('dotenv').config({path: './.env.local'});
 var lodash = require('lodash');
 var SteamApi = require('steamapi');
 var steam = new SteamApi(process.env.STEAM_API_KEY);
+var devprofile = process.env.DEV_STEAM_URL;
+
+var friendsCache = {};
 
 const coordinator = {
+    devprofile,
+    fetchUserFriends: function(user, cb) {
+        steam.resolve(user).then(uid => {
+            if(!friendsCache[uid]) {
+                console.log(`caching ${uid} friends`);
+                steam.getUserFriends(uid)
+                .then(friends => steam.getUserSummary(friends.map(friend => friend.steamID)))
+                .then(summaries => {
+                    friendsCache[uid] = summaries;
+                    cb(friendsCache[uid]);
+                });
+            } else {
+                console.log(`cached ${uid} hit`);
+                cb(friendsCache[uid]);
+            }
+        })
+        .catch(err => console.log(err));
+    },
+    searchForUserFriends: function(user, search, cb) {
+        const part = new RegExp(search, "gi");
+        this.fetchUserFriends(user, friends => {
+            console.log(part)
+            const matches = friends.filter(friend => part.test(friend.nickname));
+            cb({ friends: matches, count: matches.length })
+        });
+    },
     getIntersectingGames: function(users, cb) {
         Promise.all(users.map(user => steam.resolve(user).then(uid => steam.getUserOwnedGames(uid))))
         .then(libraries => {
--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 "use strict";
 var coordinator = require('./coordinator');
 
+const { devprofile } = coordinator;
 const users = [
     // valve employee ids for dummy data, using these will error out with no games found
     "steamcommunity.com/id/robinwalker",
     "steamcommunity.com/profiles/76561197975914763"
 ];
 
-coordinator.getIntersectingGames(users, console.log);
+//coordinator.getIntersectingGames(users, console.log);
+coordinator.getIntersectingGames([devprofile], console.log);
+//coordinator.searchForUserFriends(devprofile, 'fr', console.log);
+coordinator.fetchUserFriends(devprofile, friends => {
+    console.log(friends.map(friend => friend.nickname).filter(nick => /dog/gi.test(nick)))
+});
+
+/*
+setTimeout(() => {
+    coordinator.fetchUserFriends(devprofile, friends => {
+        console.log(friends.map(friend => friend.nickname))
+    })
+}, 10000)
+*/
